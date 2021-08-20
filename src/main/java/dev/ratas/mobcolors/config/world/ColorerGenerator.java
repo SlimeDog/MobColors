44b5951c55bf959f6e91f693082a11c0e6afe0be
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-08-20 09:45:30 +0300

    Now using tropical fish and horse variant clases for colorers for those mobs

--- src/main/java/dev/ratas/mobcolors/config/world/ColorerGenerator.java
+++ src/main/java/dev/ratas/mobcolors/config/world/ColorerGenerator.java
@@ -4,15 +4,19 @@ import org.bukkit.DyeColor;
 import org.bukkit.entity.Axolotl;
 import org.bukkit.entity.Cat;
 import org.bukkit.entity.Fox;
+import org.bukkit.entity.Horse;
 import org.bukkit.entity.Llama;
 import org.bukkit.entity.MushroomCow;
 import org.bukkit.entity.Parrot;
 import org.bukkit.entity.Rabbit;
+import org.bukkit.entity.TropicalFish;
 import org.bukkit.material.Colorable;
 
 import dev.ratas.mobcolors.coloring.DelegatingMobColorer;
 import dev.ratas.mobcolors.coloring.MobColorer;
 import dev.ratas.mobcolors.coloring.settings.ColorMap;
+import dev.ratas.mobcolors.config.HorseVariant;
+import dev.ratas.mobcolors.config.TropicalFishVariant;
 import dev.ratas.mobcolors.config.mob.MobSettings;
 import dev.ratas.mobcolors.scheduling.abstraction.Scheduler;
 
@@ -52,8 +56,18 @@ public class ColorerGenerator {
                 return new DelegatingMobColorer<Rabbit, Rabbit.Type>(scheduler, settings, (ColorMap<Rabbit.Type>) map,
                         (rabbit, type) -> rabbit.setRabbitType(type));
             case HORSE:
+                return new DelegatingMobColorer<Horse, HorseVariant>(scheduler, settings, (ColorMap<HorseVariant>) map,
+                        (horse, var) -> {
+                            horse.setColor(var.getOne());
+                            horse.setStyle(var.getTwo());
+                        });
             case TROPICAL_FISH:
-                throw new RuntimeException("SPECIAL EXCEPTION - THIS HAS NOT YET BEEN IMPLEMENTD");
+                return new DelegatingMobColorer<TropicalFish, TropicalFishVariant>(scheduler, settings,
+                        (ColorMap<TropicalFishVariant>) map, (fish, var) -> {
+                            fish.setPattern(var.getOne());
+                            fish.setBodyColor(var.getTwo());
+                            fish.setPatternColor(var.getThree());
+                        });
             default:
                 throw new IllegalStateException("No colorer defined for " + map.getApplicableEntityType());
         }
