6f3abb069a37559f998e1b8dc30700652528b6ae
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-08-28 07:32:56 +0300

    Keeping track of default world settings and using that if nothing else was specified for a world

--- src/main/java/dev/ratas/mobcolors/config/world/WorldManager.java
+++ src/main/java/dev/ratas/mobcolors/config/world/WorldManager.java
@@ -11,12 +11,16 @@ import dev.ratas.mobcolors.scheduling.abstraction.Scheduler;
 
 public class WorldManager {
     private final Map<String, WorldSettings> worldSettings = new HashMap<>();
+    private final WorldSettings defaultWorldSettings = new WorldSettings();
 
     public void addMobSettings(MobSettings settings, Scheduler scheduler) {
         for (ColorMap<?> map : settings.getAllColorMaps()) {
             for (String worldName : map.getApplicableWorlds()) {
                 WorldSettings ws = worldSettings.computeIfAbsent(worldName.toLowerCase(), (n) -> new WorldSettings());
                 ws.addScheme(map, settings, scheduler);
+            }
+            if (map.getName().equals("default")) {
+                defaultWorldSettings.addScheme(map, settings, scheduler);
             }
         }
     }
@@ -26,7 +30,7 @@ public class WorldManager {
     }
 
     public WorldSettings getWorldSettings(String worldName) {
-        return worldSettings.get(worldName.toLowerCase());
+        return worldSettings.getOrDefault(worldName.toLowerCase(), defaultWorldSettings);
     }
 
     public void clear() {
