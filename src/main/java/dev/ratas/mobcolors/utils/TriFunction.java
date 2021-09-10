aa34e20751641f58aa3d1f6c2d36f9f21bcef02d
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-09-10 08:52:58 +0300

    Added tri function functional interface

---
+++ src/main/java/dev/ratas/mobcolors/utils/TriFunction.java
@@ -0,0 +1,17 @@
+package dev.ratas.mobcolors.utils;
+
+import java.util.Objects;
+
+import com.google.common.base.Function;
+
+@FunctionalInterface
+public interface TriFunction<A, B, C, R> {
+
+    R apply(A a, B b, C c);
+
+    default <V> TriFunction<A, B, C, V> andThen(Function<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+        return (A a, B b, C c) -> after.apply(apply(a, b, c));
+    }
+
+}
