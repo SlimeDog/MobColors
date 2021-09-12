c178d9bc1570e44621629c14004c9d06c4f6a028
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-09-12 08:41:38 +0300

    Added test for random tropical fish variants

---
+++ src/test/java/dev/ratas/mobcolors/config/variants/TestTropicalFishRandomVariant.java
@@ -0,0 +1,22 @@
+package dev.ratas.mobcolors.config.variants;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import dev.ratas.mobcolors.config.TropicalFishVariant;
+
+public class TestTropicalFishRandomVariant {
+
+    @Test
+    public void test_TropicalFishRandomVariantVaries() {
+        String varName = "random";
+        TropicalFishVariant var1 = TropicalFishVariant.valueOf(varName);
+        TropicalFishVariant var2 = TropicalFishVariant.valueOf(varName);
+        if (var2 == var1) { // low chance, but lets roll the dice again
+            var2 = TropicalFishVariant.valueOf(varName);
+        }
+        Assertions.assertNotSame(var1, var2, "Random tropical fish are the same");
+        Assertions.assertNotEquals(var1, var2, "Random tropical fish are equal");
+    }
+
+}
