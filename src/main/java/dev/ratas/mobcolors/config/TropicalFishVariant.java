4dde222b95fd784b79dc5d6335bb9674d09dc5fb
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-09-12 08:38:42 +0300

    Added random handling to tropical fish variant parsing

--- src/main/java/dev/ratas/mobcolors/config/TropicalFishVariant.java
+++ src/main/java/dev/ratas/mobcolors/config/TropicalFishVariant.java
@@ -1,11 +1,14 @@
 package dev.ratas.mobcolors.config;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.bukkit.DyeColor;
 import org.bukkit.entity.TropicalFish;
 
 import dev.ratas.mobcolors.config.variants.ThreeTypeComplexVariant;
 
 public class TropicalFishVariant extends ThreeTypeComplexVariant<TropicalFish.Pattern, DyeColor, DyeColor> {
+    private static final String RANDOM_STATE = "random";
     private static final InstanceTracker<TropicalFish.Pattern, InstanceTracker<DyeColor, InstanceTracker<DyeColor, TropicalFishVariant>>> INSTANCE_TRACKER = InstanceTracker
             .getTripleTracker((p, c1, c2) -> new TropicalFishVariant(p, c1, c2));
     public static final String DELIMITER = "/";
@@ -15,6 +18,9 @@ public class TropicalFishVariant extends ThreeTypeComplexVariant<TropicalFish.Pa
     }
 
     public static TropicalFishVariant valueOf(String name) {
+        if (name.equals(RANDOM_STATE)) {
+            return randomVariant();
+        }
         String[] keys = name.split(DELIMITER);
         int nr = keys.length;
         if (nr != 3) {
@@ -22,6 +28,19 @@ public class TropicalFishVariant extends ThreeTypeComplexVariant<TropicalFish.Pa
                     "Wrong number of different types in key '" + name + "'. Expected " + 3 + " but got " + nr);
         }
         return getVariant(TropicalFish.Pattern.valueOf(keys[0]), DyeColor.valueOf(keys[1]), DyeColor.valueOf(keys[2]));
+    }
+
+    public static TropicalFishVariant randomVariant() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        TropicalFish.Pattern[] values = TropicalFish.Pattern.values();
+        int nr = random.nextInt(values.length);
+        TropicalFish.Pattern pattern = values[nr];
+        DyeColor[] dyeValues = DyeColor.values();
+        nr = random.nextInt(dyeValues.length);
+        DyeColor color1 = dyeValues[nr];
+        nr = random.nextInt(dyeValues.length);
+        DyeColor color2 = dyeValues[nr];
+        return getVariant(pattern, color1, color2);
     }
 
     @Override
