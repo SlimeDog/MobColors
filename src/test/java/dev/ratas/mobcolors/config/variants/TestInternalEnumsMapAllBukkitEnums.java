06b0e4a667ad3a402f47cf9b2550298210db4ff2
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-09-12 09:28:53 +0300

    Added tests for making sure all internal enums correctly and in a one-to-one way map to bukkit enums

---
+++ src/test/java/dev/ratas/mobcolors/config/variants/TestInternalEnumsMapAllBukkitEnums.java
@@ -0,0 +1,39 @@
+package dev.ratas.mobcolors.config.variants;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import dev.ratas.mobcolors.config.mob.MobTypes;
+
+public class TestInternalEnumsMapAllBukkitEnums {
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForTest")
+    public void test_InternalEnumMapsAllBukkitEnumValues(Class<Enum<? extends MobTypeVariant<?>>> clazz) {
+        Set<Object> usedBukkitVariants = new HashSet<>();
+        for (Object internal : clazz.getEnumConstants()) {
+            Object bukkitVariant = ((MobTypeVariant<?>) internal).getBukkitVariant();
+            Assertions.assertTrue(usedBukkitVariants.add(bukkitVariant), "Multiple mappings for " + bukkitVariant);
+            if (internal != DyeVariant.DEFAULT) { // hacky solution
+                Assertions.assertNotNull(bukkitVariant, "No bukkit mapping for " + internal);
+            }
+        }
+        Assertions.assertFalse(usedBukkitVariants.isEmpty(), "No mappings for " + clazz.getSimpleName());
+        Class<?> bukkitEnumClass = usedBukkitVariants.iterator().next().getClass();
+        for (Object bukkit : bukkitEnumClass.getEnumConstants()) {
+            Assertions.assertTrue(usedBukkitVariants.contains(bukkit), "No internal enum for " + bukkit);
+        }
+    }
+
+    private static List<Class<? extends MobTypeVariant<?>>> provideArgumentsForTest() {
+        return new ArrayList<>(MobTypes.VARIANT_MOB_TYPES.keySet());
+
+    }
+
+}
